e2c1adbae834d37d6d6990a4503b1d0cc9013279
Eric Dahl <edahl@groupon.com>
2014-01-21 10:41:23 -0600

    initial commit

---
+++ audit-comments-metrics.php
@@ -0,0 +1,200 @@
+#!/usr/bin/env php
+<?php
+
+include("bootstrap.php");
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$start = time();
+
+$job = $pheanstalk->reserve();
+//$cb = new Couchbase("127.0.0.1:8091", "", "", "tickets");
+
+$log = new Logger('testcomments.php');
+$log->pushHandler(new StreamHandler(__DIR__ . '/data.log', Logger::WARNING));
+
+
+//echo $job->getData();
+
+do {
+
+	$jobObj = json_decode($job->getData());
+
+	echo "Starting:". $jobObj->id." | ";
+
+	echo "Audits: ";
+
+	$user = $desks[$jobObj->account]['api_user'];
+	$pass = $desks[$jobObj->account]['api_key'];
+	b:
+	$response = \Httpful\Request::get('https://'.$jobObj->account.'/api/v2/tickets/'.$jobObj->id.'/audits.json?include=tickets')
+	->authenticateWith($user.'/token', $pass)
+	// ->sendsJson()
+	->expectsJson()
+	->send();
+
+	if ($response->code ==429) {
+		$myPush = new Push();
+		$myPush->setMessage('audits');
+		$myPush->setTitle('429'); // Optional
+		$pushManager->push($myPush);
+
+		$log->addWarning('Error 429: Line 33');
+
+		echo "Waiting ".$response->headers["Retry-After"]." seconds".PHP_EOL;
+		sleep($response->headers["Retry-After"]);
+		goto b;
+	}
+	elseif ($response->code == 404) {
+		$log->addWarning('Error 404: Line 43');
+
+		$pheanstalk->delete($job);
+		goto c;
+	}
+	elseif ($response->code != 200) {
+		echo "code: ".$response->code;
+		var_dump($jobObj);die();
+	}
+
+	echo "Fetched, ";
+
+	$MongoTicket = (object) $response->body->tickets[0];
+
+
+	$MongoTicket->created_at = new MongoDate(strtotime($MongoTicket->created_at));
+	$MongoTicket->updated_at = new MongoDate(strtotime($MongoTicket->updated_at));
+
+	unset($MongoTicket->fields);
+
+	foreach ($response->body->audits as $key=>$audit) {
+		$response->body->audits[$key]->created_at = new MongoDate(strtotime($response->body->audits[$key]->created_at));
+
+	}
+	$MongoTicket->_id = $MongoTicket->id;
+
+	$MongoTicket->audits = $response->body->audits;
+	echo "Compiled | ";
+	echo "Comments: ";
+
+	d:
+	unset($response);
+	$response = \Httpful\Request::get('https://'.$jobObj->account.'/api/v2/tickets/'.$jobObj->id.'/comments.json')
+	->authenticateWith($user.'/token', $pass)
+	// ->sendsJson()
+	->expectsJson()
+	->send();
+
+	if ($response->code ==429) {
+		$myPush = new Push();
+		$myPush->setMessage('comments');
+		$myPush->setTitle('429'); // Optional
+		$pushManager->push($myPush);
+
+		$log->addWarning('Error 429: Line 74');
+
+		echo "Waiting ".$response->headers["Retry-After"]." seconds".PHP_EOL;
+		sleep($response->headers["Retry-After"]);
+		goto d;
+	}
+	elseif ($response->code == 404) {
+		$pheanstalk->delete($job);
+		// echo "*";
+
+		goto c;
+	}
+	elseif ($response->code != 200) {
+		echo "code: ".$response->code;
+		var_dump($jobObj);die();
+	}
+	echo "Fetched, ";
+
+	foreach ($response->body->comments as $key=>$comment) {
+		$response->body->comments[$key]->created_at = new MongoDate(strtotime($response->body->comments[$key]->created_at));
+	}
+
+	$MongoComments = (object) $response->body->comments;
+
+	$MongoTicket->comments = $MongoComments;
+	echo "Compiled | ";
+	echo "Metrics: ";
+	e:
+	unset($response);
+	$response = \Httpful\Request::get('https://'.$jobObj->account.'/api/v2/tickets/'.$jobObj->id.'/metrics.json')
+	->authenticateWith($user.'/token', $pass)
+	// ->sendsJson()
+	->expectsJson()
+	->send();
+
+	if ($response->code ==429) {
+		$myPush = new Push();
+		$myPush->setMessage('comments');
+		$myPush->setTitle('429'); // Optional
+		$pushManager->push($myPush);
+
+		$log->addWarning('Error 429: Line 74');
+
+		echo "Waiting ".$response->headers["Retry-After"]." seconds".PHP_EOL;
+		sleep($response->headers["Retry-After"]);
+		goto e;
+	}
+	elseif ($response->code == 404) {
+		$pheanstalk->delete($job);
+		// echo "*";
+
+		goto c;
+	}
+	elseif ($response->code != 200) {
+		echo "code: ".$response->code;
+		var_dump($jobObj);die();
+	}
+	echo "Fetched, ";
+
+	unset($response->body->ticket_metric-> url);
+	unset($response->body->ticket_metric-> id);
+	unset($response->body->ticket_metric-> ticket_id);
+	unset($response->body->ticket_metric-> created_at);
+	unset($response->body->ticket_metric-> updated_at);
+
+	foreach ( $response->body->ticket_metric as $key=>$metric) {
+		if (isset($metric)) {
+			if (strpos($key,'_at') !== false) {
+				$metric = new MongoDate(strtotime($metric));
+			}
+			$MongoTicket -> $key = $metric;
+		}
+	}
+
+
+
+	echo "Compiled | ";
+
+	if ($collection->save($MongoTicket)) {
+		echo "Saved".PHP_EOL;
+
+
+		//  $cb->set($MongoTicket->id, json_encode($MongoTicket) );
+
+		$pheanstalk->delete($job);
+
+	}
+	else {
+		echo "Not saved".PHP_EOL;
+	}
+
+	c:
+
+	if (time()-$start >= 600) {
+		die();
+	}
+
+}
+while ( $job = $pheanstalk->reserve());
+
+
+
+
+
+
+
+
+?>
---
+++ bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+date_default_timezone_set('America/Chicago');
+
+require ('vendor/autoload.php');
+
+use Jasny\MySQL\DB;
+use Sly\PushOver\Model\Push;
+use Sly\PushOver\PushManager;
+use Simplon\Config\Config;
+
+$config = Config::getInstance()->setConfigPath(__DIR__ . '/config.php');
+$DBConfig = $config->getConfigByKeys(['database', 'mysql']);
+
+
+//configure MySQL connection
+$csb = new DB($DBConfig["server"], $DBConfig["username"], $DBConfig["password"], $DBConfig["database"], $DBConfig["port"]);
+
+$items = $csb->fetchAll("SELECT * FROM `zendeskulator_desks`;");
+
+$desks = array();
+foreach ($items as $item) {
+	$desks[$item["domain"]] = $item;
+}
+
+//configure pheanstalk
+$pheanstalk = new Pheanstalk_Pheanstalk($config->getConfigByKeys(['beanstalkd', 'server']));
+$pheanstalk->watch('audits-mongo-test');
+$pheanstalk->ignore('default');
+$pheanstalk->useTube('audits-mongo-test');
+
+
+
+//config mongodb connection
+$m = new MongoClient();
+// select a database
+$db = $m->Zendesk;
+// select a collection (analogous to a relational database's table)
+$collection = $db->Tickets;
+
+$pushManager = new PushManager('uurwWV2RzVq9YJBi2ozg6KJRqhxVVh', 'ahxqMWVtsQmLTmhL8kbSGfwBViufat');
+
+
